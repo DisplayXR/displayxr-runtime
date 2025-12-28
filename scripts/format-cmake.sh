86a3786cffed2d01b97364ee458f1cc67885e517
Jakob Bornecrantz <tbornecrantz@nvidia.com>
2025-12-28 11:16:16 +0000

    external/imgui: Create own CMake file
    Part-of: <https://gitlab.freedesktop.org/monado/monado/-/merge_requests/2692>

--- scripts/format-cmake.sh
+++ scripts/format-cmake.sh
@@ -22,6 +22,7 @@ fi
         doc \
         src/xrt \
         src/external/CMakeLists.txt \
+        src/external/imgui/CMakeLists.txt \
         src/external/openxr_includes/CMakeLists.txt \
         src/CMakeLists.txt \
         tests \
